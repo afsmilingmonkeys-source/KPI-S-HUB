db0c575245e3d1fcd46920d90335e026c797b600
afsmilingmonkeys-source <af.smiling.monkeys@gmail.com>
2025-12-05 01:34:49 +0100

    Fix console log message for KPI-S-HUB loading

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 \pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
 
 \f0\fs24 \cf0 // KPI-S-HUB - Script principal\
-console.log('KPI-S-HUB cargado correctamente');\
+console.log('KPI-S-HUB cargado  correctamente');\
 \
 // Funci\'f3n b\'e1sica para evitar error si usas jQuery o algo similar\
 document.addEventListener('DOMContentLoaded', function() \{\
